0214d4fa89ce5ee6cf5e2331c4f402be2dbc4697
angelp495 <angeljpa95@gmail.com>
2021-07-06 16:24:11 -0500

    show & hide menu mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const d = document;
+const d = document,
+  $btnMenu = d.getElementById("menu-btn"),
+  $menu = d.getElementById("menu-items");
 
 d.addEventListener("click", (e) => {
+  /* show or hide menu */
+  if (e.target == $btnMenu || e.target.matches("ul *")) {
+    /* AGREGAR ANIMACIÓN PARA DESAPARECER MENU BIEN BONITO */
+
+    $menu.classList.toggle("active");
+  }
+
+  /* Expand or contract items */
   if (e.target.matches(".dropdown *")) {
     e.target.parentNode.classList.toggle("active");
 
